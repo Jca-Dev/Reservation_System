044622cd6fb4ed3be1c0bdf419e565519f678492
John <John.Arscott@hotmail.com>
2022-10-30 17:03:25 +0000

    create base.html css

--- reservation_manager/models.py
+++ reservation_manager/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Base(models.Model):
-    
+
     def __str__(self):
         return self.name
--- reservation_manager/urls.py
+++ reservation_manager/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Homepage.as_view(), name='home'),
-    path('/reservation_page', views.Reservations.as_view(), name='reservations'),
-    path('/menu', views.Menu.as_view(), name='menu'),
+    path('reservation_page', views.Reservations.as_view(), name='reservations'),
+    path('menu', views.Menu.as_view(), name='menu'),
 ]
