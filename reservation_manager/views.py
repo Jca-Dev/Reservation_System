8d2763b3f0b546a5031d9542efada395b428abe3
John <John.Arscott@hotmail.com>
2022-10-30 12:04:32 +0000

    create base.html frame & link

--- reservation_manager/views.py
+++ reservation_manager/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Base
 
-# Create your views here.
+
+class Homepage(ListView):
+    model = Base
+    template_name = 'base.html'
